c71010752d09c78ab23f8ccbf91bd0388fa069c0
Brian Noh <noh24@ymail.com>
2022-12-03 23:25:00 -0500

    add for loop for how many leap years user experienced

--- GroceryList.cs
+++ GroceryList.cs
@@ -1,24 +1,46 @@
 using System;
 
-class GroceryList
+// class GroceryList
+// {
+//   static void Main()
+//   {
+//     string[] groceryList = { "eggs", "milk", "cheese", "ice cream" };
+//     int[] groceryListPrices = { 3, 4, 5, 6 };
+
+//     int counter = 1;
+//     Console.WriteLine("Grocery List:");
+//     foreach (string groceryItem in groceryList)
+//     {
+//       Console.WriteLine($"{counter}. {groceryItem}");
+//       counter++;
+//     }
+//     int total = 0;
+//     foreach (int itemPrice in groceryListPrices)
+//     {
+//       total += itemPrice;
+//     }
+//     Console.WriteLine($"Total Price: ${total}");
+//   }
+// }
+
+class LeapYear
 {
   static void Main()
   {
-    string[] groceryList = { "eggs", "milk", "cheese", "ice cream" };
-    int[] groceryListPrices = { 3, 4, 5, 6 };
-
-    int counter = 1;
-    Console.WriteLine("Grocery List:");
-    foreach (string groceryItem in groceryList)
+    Console.WriteLine("Enter your birth year");
+    string stringBirthYear = Console.ReadLine();
+    int birthYear = int.Parse(stringBirthYear);
+    Console.WriteLine("These are all the leap years you've lived through:");
+    int counter = 0;
+    for (int year = birthYear; year <= 2022; year++)
     {
-      Console.WriteLine($"{counter}. {groceryItem}");
-      counter++;
+      if (year % 4 == 0)
+      {
+        Console.WriteLine(year);
+        counter++;
+      }
     }
-    int total = 0;
-    foreach (int itemPrice in groceryListPrices)
-    {
-      total += itemPrice;
-    }
-    Console.WriteLine($"Total Price: ${total}");
+    Console.WriteLine($"You have lived through {counter} leap years.");
+    // leap years that occured since birth
   }
 }
